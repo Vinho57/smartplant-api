daf179bb33dbbb91838a193f325723c29e88eac9
Vinho <friedl.kevin57@gmail.com>
2025-06-29 09:43:56 +0200

    Plots mit echtem Datenabruf per pot_id integriert

--- routes/plots/plots.py
+++ routes/plots/plots.py
@@ -12,22 +12,27 @@ from routes.plots.sonnenstunden import generate_sonnenstunden_plot
 plots_blueprint = Blueprint('plots', __name__)
 
 
-def render_plot(generate_plot_func):
-    return Response(generate_plot_func(df=None, use_dummy=True), mimetype='text/html')
+def render_plot(generate_plot_func, pot_id):
+    df = get_all_measurements_for_today(pot_id)
+    return Response(generate_plot_func(df=df, use_dummy=False), mimetype='text/html')
 
 
 @plots_blueprint.route("/plots/temperature", methods=["GET"])
 def render_temperature_plot():
-    return render_plot(generate_temperatur_plot)
+    pot_id = int(request.args.get("pot_id", 1))
+    return render_plot(generate_temperatur_plot, pot_id)
 
 @plots_blueprint.route("/plots/soil", methods=["GET"])
 def render_soil_plot():
-    return render_plot(generate_bodenfeuchtigkeit_plot)
+    pot_id = int(request.args.get("pot_id", 1))
+    return render_plot(generate_bodenfeuchtigkeit_plot, pot_id)
 
 @plots_blueprint.route("/plots/luftfeuchtigkeit", methods=["GET"])
 def render_luftfeuchtigkeit_plot():
-    return render_plot(generate_luftfeuchtigkeit_plot)
+    pot_id = int(request.args.get("pot_id", 1))
+    return render_plot(generate_luftfeuchtigkeit_plot, pot_id)
 
 @plots_blueprint.route("/plots/sunlight", methods=["GET"])
 def render_sunlight_plot():
-    return render_plot(generate_sonnenstunden_plot)
+    pot_id = int(request.args.get("pot_id", 1))
+    return render_plot(generate_sonnenstunden_plot, pot_id)
